6ca400d5d77a2ddb36f612381263285c01846f3a
raghavluthra10 <77231964+raghavluthra10@users.noreply.github.com>
2022-06-24 00:36:01 +0530

    added update hotel feature as a hotel_owner

--- controllers/hotelOwnerController.js
+++ controllers/hotelOwnerController.js
@@ -31,6 +31,14 @@ const getMyHotels = async (req, res) => {
   }
 };
 
+const getMySingleHotel = async () => {
+  try {
+    // take in ID and return that hotel
+  } catch (error) {
+    return serverErrorResponse(res, 500, error);
+  }
+};
+
 const createNewHotel = async (req, res) => {
   try {
     const role = req.role;
@@ -56,15 +64,60 @@ const createNewHotel = async (req, res) => {
       user_id: id,
     });
 
-    return res.json({
-      message: "this is api for creating a new hotel | POST req",
-    });
+    return successResponse(res, 200, "New hotel created!!!");
   } catch (error) {
     return serverErrorResponse(res, 500, error);
   }
 };
 
+// edit
+const updateHotel = async (req, res) => {
+  try {
+    const role = req.role;
+    const id = req.user;
+
+    const hotel_id = req.query.hotel_id;
+
+    const { hotel_name, description, bhk, price_per_night } = req.body;
+
+    console.log(
+      "updateHotel= ==== = = ",
+      req.role,
+      req.user,
+      req.body,
+      hotel_id
+    );
+
+    if (role !== "hotel_owner") {
+      return unauthorized(res);
+    }
+
+    if (!hotel_name || !description || !bhk || !price_per_night) {
+      return errorResponse(res, 422, "Please send all parameters");
+    }
+
+    const updateHotel = await db.hotel.update(
+      {
+        hotel_name,
+        description,
+        bhk,
+        price_per_night,
+      },
+      {
+        where: { id: hotel_id },
+      }
+    );
+
+    return successResponse(res, 200, updateHotel);
+  } catch (error) {
+    return serverErrorResponse(res, 500, error);
+  }
+};
+
+// delete
+
 module.exports = {
   getMyHotels,
   createNewHotel,
+  updateHotel,
 };
--- routes/hotelOwnerRoutes.js
+++ routes/hotelOwnerRoutes.js
@@ -4,9 +4,11 @@ const router = express.Router();
 const {
   getMyHotels,
   createNewHotel,
+  updateHotel,
 } = require("../controllers/hotelOwnerController");
 
 router.get("/getHotels", getMyHotels);
 router.post("/createNewHotel", createNewHotel);
+router.put("/updateHotel", updateHotel);
 
 module.exports = router;
